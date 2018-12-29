3ecee7f2326ba6b4e250a20bec832e792c452e5a
Andreas Mellstrom <Arachna.m@hotmail.se>
2018-12-29 12:49:34 +0100

    Init

---
+++ NETDatingApp/NETDatingApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NETDatingApp.Startup))]
+namespace NETDatingApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
