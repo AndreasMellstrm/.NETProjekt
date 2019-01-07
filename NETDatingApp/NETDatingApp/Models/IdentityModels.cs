c0e7f9b241104a124f1e0616f5b3773242df146a
Andreas Mellstrom <Arachna.m@hotmail.se>
2019-01-07 18:51:12 +0100

    Breakthrough friends

--- NETDatingApp/NETDatingApp/Models/IdentityModels.cs
+++ NETDatingApp/NETDatingApp/Models/IdentityModels.cs
@@ -1,21 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
-namespace NETDatingApp.Models
-{
+namespace NETDatingApp.Models {
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser {
-        
-       public int ProfileID { get; set; }
-       public virtual PersonProfile PersonProfile { get; set; }
-        
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
+
+        public int ProfileID { get; set; }
+        public virtual PersonProfile PersonProfile { get; set; }
+
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager) {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
@@ -23,18 +23,19 @@ namespace NETDatingApp.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser> {
 
-        public DbSet<PersonProfile>  PersonProfiles { get; set; }
+        public DbSet<PersonProfile> PersonProfiles { get; set; }
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
+            : base("DefaultConnection", throwIfV1Schema: false) {
         }
-
-
-        public static ApplicationDbContext Create()
-        {
+        protected override void OnModelCreating(DbModelBuilder modelBuilder) {
+           base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Friends>().HasRequired(c => c.ProfileA).WithMany(u => u.ProfileAFriends).HasForeignKey(c => c.ProfileAId).WillCascadeOnDelete(false);
+            modelBuilder.Entity<Friends>().HasRequired(c => c.ProfileB).WithMany(u => u.ProfileBFriends).HasForeignKey(c => c.ProfileBId).WillCascadeOnDelete(false);
+        }
+       
+        public static ApplicationDbContext Create() {
             return new ApplicationDbContext();
         }
     }
--- NETDatingApp/NETDatingApp/Models/PersonModels.cs
+++ NETDatingApp/NETDatingApp/Models/PersonModels.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Identity;
@@ -20,6 +21,13 @@ namespace NETDatingApp.Models {
         public string Bio { get; set; }
         public string ProfileImg { get; set; }
         public virtual ICollection<ApplicationUser> ApplicationUsers { get; set; }
+        
+        [InverseProperty("ProfileA")]
+        public virtual ICollection<Friends> ProfileAFriends { get; set; }
+
+        [InverseProperty("ProfileB")]
+        public virtual ICollection<Friends> ProfileBFriends { get; set; }
+
 
         public PersonProfile() {
 
@@ -32,6 +40,32 @@ namespace NETDatingApp.Models {
             this.Bio = Bio;
             this.ProfileImg = ProfileImg;
             ApplicationUsers = new HashSet<ApplicationUser>();
+            ProfileAFriends = new HashSet<Friends>();
+            ProfileBFriends = new HashSet<Friends>();
+
         }
+
     }
+
+    public class Friends {
+
+        [Key, Column(Order = 0)]
+        public int ProfileAId { get; set; }
+
+        [Key, Column(Order = 1)]
+        public int ProfileBId { get; set; }
+
+        [ForeignKey("ProfileAId")]
+        [InverseProperty("ProfileAFriends")]
+        public virtual PersonProfile ProfileA { get; set; }
+
+        [ForeignKey("UserBId")]
+        [InverseProperty("ProfileBFriends")]
+        public virtual PersonProfile ProfileB { get; set; }
+
+        public bool IsFriends { get; set; }
+
+    }
+    
+
 }
