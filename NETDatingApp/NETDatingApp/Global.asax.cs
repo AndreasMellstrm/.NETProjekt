9c851d906c1656e71408d2298ec4420a5feabf5b
ludvig lundh <l.lundh@hotmail.com>
2019-01-08 10:58:45 +0100

    ADDERADE webAPi med nuget samt config

--- NETDatingApp/NETDatingApp/Global.asax.cs
+++ NETDatingApp/NETDatingApp/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
 
 namespace NETDatingApp
 {
@@ -16,6 +17,7 @@ namespace NETDatingApp
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            System.Web.Http.GlobalConfiguration.Configure(WebApiConfig.Register);
 
         }
     }
